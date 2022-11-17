70bd2534394f0eb8e06f37f06864c37e4949958d
rzatalibov555 <sethubpro@gmail.com>
2022-11-17 23:01:05 +0400

    test

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+
+// function car(brand, model, motor, fuel){
+//     console.log( "Modeli: "+model+". Motoru: "+motor+", yanacagi: "+fuel+" Menim mawinim "+brand)
+// }
+
+// car("AUDI","A6","4.4","benzin")
+// car("Mercedes","E280","2.8","benzin")
+// car("Toyota","Prado","3.5","Dizel")
+
+
+// function hobby(age,ad,profession,soyad = "Talibov"){
+//     console.log("Menim adim "+ad+" "+soyad+". Menim hobbim "+profession+". Yawim "+age)
+// }
+
+// hobby("21","Xan","bilyard oynamaq","Huseynov")
+// hobby("31","Rza","bilyard oynamaq")
+
+
+// function person(ad,soyad = "Huseynov",Status = "Telebe"){
+//     console.log("Ad: "+ ad +", Soyad: "+ soyad +". Status: "+Status)
+// }
+
+// person("Rza", "Talibov")
+// person("Xan")
+// person("Xan")
+// person("Xan")
+// person("Xan")
+// person("Xan")
+
+// person("Xan", "Huseynov","Muellim")
+
+
+function chBlue(e){
+    console.log(e)
+    e.style.background = "blue" 
+}
+
+function chGreen(e){
+    e.style.background = "green" 
+}
+
+function chRed(e){
+    e.style.background = "red" 
+}
+
+function chPurple(e){
+    e.style.display = "none" 
+}
